113d809eb326f85be181b2cc09e476b5b89d7b88
Abiddaris <abiddaris1101@gmail.com>
2025-01-04 13:49:30 +0800

    refactor: Move code that manage app list to its own file

--- ui.js
+++ ui.js
@@ -8,6 +8,7 @@ const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const {get_categories} = require('./app');
 const Display = require('./display');
+const AppUI = require('./appui');
 
 class UI {
     constructor(applet) {
@@ -24,7 +25,7 @@ class UI {
         this.actor.connect('button-release-event', this._onActorClicked.bind(this));
 
         this.sidebar = new SideBar(this, this.applet);
-        this.appList = createAppListUI(this, this.applet, get_categories());
+        this.appList = AppUI.createAppListUI(this, this.applet, get_categories());
 
         this.actor.add_actor(this.appList);
         this.actor.add_actor(this.sidebar.actor);
@@ -92,103 +93,6 @@ class UI {
         return this._menu != undefined;
     }
 
-}
-
-function createAppListUI(ui, applet, categories) {
-    const apps = _flattenCategories(categories)
-    apps.sort((a, b) => a.get_name().localeCompare(b.get_name(), undefined,
-                                      {sensitivity: "base", ignorePunctuation: true}));
-    const scrollView = new St.ScrollView({
-        width: ui.getMenuWidth(),
-        height: ui.getMenuHeight(),
-        style: "padding-top: 7px;"
-    });
-    const applications = new St.BoxLayout({ 
-        vertical: true
-    }); 
-
-    apps.forEach(app => applications.add(new AppItemLayout(ui, applet, app).actor))
-
-    scrollView.add_actor(applications);
-
-    return scrollView;
-}
-
-function _flattenCategories(categories) {
-    let apps = [];
-    categories.forEach(category => {
-        category.apps.forEach(app => apps.push(app));
-    });
-
-    return apps;
-}
-
-class AppItemLayout {
-
-    base_list_style = "padding-left: 9px; padding-top: 9px; padding-bottom: 9px; ";
-
-    constructor(ui, applet, app) {
-        this.applet = applet;
-        this.app = app;
-        this.ui = ui;
-
-        this.actor = new St.BoxLayout({
-             reactive: true, 
-             style: this.base_list_style
-        });
-        this.label = new St.Label({
-            style: "padding-left: 7px"
-        });
-
-        const clutterText = this.label.get_clutter_text();
-        clutterText.set_markup(app.get_name());
-
-        this.actor.add(this._get_icon(app));
-        this.actor.add(this.label, {
-            y_fill: false,
-            y_align: St.Align.MIDDLE
-        });
-
-        this.actor.connect('button-release-event', this._onReleaseEvent.bind(this));
-        this.actor.connect('enter-event', this._enterEvent.bind(this));
-
-        this.actor.connect('leave-event', () => {
-            if (ui.isMenuOpen()) {
-                return Clutter.EVENT_PROPAGATE;
-            }
-
-            // Reset the background color when the mouse leaves
-            this.actor.style = this.base_list_style + "transition: background-color 0.3s ease-in-out;";
-        });
-    }
-
-    _enterEvent() {
-        if (this.ui.isMenuOpen()) {
-            return Clutter.EVENT_PROPAGATE;
-        }
-
-       this.actor.style = this.base_list_style + "background-color: #222222; transition: background-color 0.3s ease-in-out;";
-    }
-
-    _onReleaseEvent(actor, event) {
-        if (this.ui.isMenuOpen()) {
-            this.ui.closeMenu();
-            this._enterEvent();
-
-            return Clutter.EVENT_STOP;
-        }
-
-        if (event.get_button() === 1) {
-            this.app.open_new_window(-1);
-            this.applet.closeMenu();
-        }
-
-        return Clutter.EVENT_STOP;
-    }
-
-    _get_icon(app) {
-        return app.create_icon_texture(30);
-    }
 }
 
 class SideBar {
@@ -338,7 +242,7 @@ class SideBar {
     }
 
     _onLeaveEvent(actor, event) {
-        if (this.actor.contains(event.get_related())) {
+        if (event != undefined && this.actor.contains(event.get_related())) {
             return;
         }
 
